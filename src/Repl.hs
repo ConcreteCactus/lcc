16671b51bd3ebcca4e104e249254b97e96ce3964
ConcreteCactus <aron.harnasi@gmail.com>
2023-06-25 16:37:06 +0200

    fixed repl

--- src/Repl.hs
+++ src/Repl.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE TupleSections #-}
+
 module Repl (repl) where
 
 import Errors
@@ -11,26 +13,35 @@ data Command = Exit
 
 repl :: FilePath -> IO ()
 repl file = do
-  (progE, env) <- loadStep file
-  case progE of
+  loadResult <- loadStep file
+  case loadResult of
     Left err -> putStrLn $ "Couldn't load file: " ++ file ++ "\n" ++ show err ++ "\n"
-    Right prog -> do
+    Right (env, prog) -> do
       print prog
-      replAgain prog
+      replAgain prog env
   where
-    replAgain :: SemProgram -> IO ()
-    replAgain prog = do
+    replAgain :: SemProgram -> Env -> IO ()
+    replAgain prog env = do
       line <- readStep
       case commandStep line of
         Just Exit -> return ()
         Nothing -> do
-          let evaluated = evalStep prog line
-          printStep $ evalStep prog line
-          replAgain prog
+          let evalResult = evalStep prog env line
+          case evalResult of
+            Left e -> printStep $ Left e
+            Right (newEnv, evaluated) -> do
+              printStep $ Right evaluated
+              replAgain prog newEnv
 
-loadStep :: FilePath -> IO (Either CompilerError SemProgram, Env)
+loadStep :: FilePath -> IO (Either CompilerError (Env, SemProgram))
 loadStep path = do
-    
+  progS <- readFile path
+  let progE = parseProgramSingleError progS
+  case progE of
+    Left e -> return $ Left e
+    Right prog -> do
+      let (env, loaded) = runState (createSemanticProgramS prog) startingEnv
+      return $ (env,) <$> loaded
 
 readStep :: IO String
 readStep = do
@@ -42,11 +53,17 @@ commandStep :: String -> Maybe Command
 commandStep ":q" = Just Exit
 commandStep _ = Nothing
 
-evalStep :: SemProgram -> String -> Either (Either CompilerError RuntimeError) EvaluatedExpression
-evalStep prog expS = do
+evalStep ::
+  SemProgram ->
+  Env ->
+  String ->
+  Either (Either CompilerError RuntimeError) (Env, EvaluatedExpression)
+evalStep prog env expS = do
   syntactic <- sinkL $ parseExpression expS
-  semantic <- sinkL $ createSemanticExpression syntactic
-  sinkR $ evaluateSafe prog 1000 semantic
+  let (newEnv, semanticE) = runState (createSemanticExpressionS syntactic) env
+  semantic <- sinkL semanticE
+  evaluated <- sinkR $ evaluateSafe prog 1000 semantic
+  return (newEnv, evaluated)
 
 printStep :: Either (Either CompilerError RuntimeError) EvaluatedExpression -> IO ()
 printStep eval = putStrLn (show eval ++ "\n")
